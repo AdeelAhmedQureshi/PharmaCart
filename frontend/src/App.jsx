19d261916d992750c252d50fc9211e1c9e4b6156
adeel localrepo <adeellocalrepo@.com>
2025-04-25 14:36:49 +0500

    corrected merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,11 @@ import { Footer } from "../Components/Footer";
 import React from "react";
 import { Home } from "../Components/Pages/Home";
 import { SingleProduct } from "../Components/SingleProduct";
+import Dashboard from "../Components/Dashboard";
+import AddProduct from "../Components/AddProduct";
+import DeleteProduct from "../Components/DeleteProduct";
+import UpdateProduct from "../Components/UpdateProduct";
+
 import { useState } from "react";
 function App() {
   const [searchText, setSearchText] = useState("");
@@ -22,6 +27,11 @@ function App() {
           <Route path="/products/:id" element={<SingleProduct/>}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}>
+          </Route>  
+          <Route path="/addproduct" element={<AddProduct />}></Route>        
+          <Route path="/deleteproduct" element={<DeleteProduct />}></Route>
+          <Route path="/updateproduct" element={<UpdateProduct />}></Route>
         </Routes>
         <Footer></Footer>
       </BrowserRouter>
