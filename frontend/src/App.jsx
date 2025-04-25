9777888128cfb280fd1ead75f9ff4de7c44a2d57
adeel localrepo <adeellocalrepo@.com>
2025-04-25 12:07:07 +0500

    Added dashboad and Add,Delete,Update Product code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import { Navbar } from '../Components/Navbar'
 import { Login } from '../Components/LogIn'
 import { SignUp } from '../Components/SignUp'
+import Dashboard from '../Components/Dashboard';
+import AddProduct from '../Components/AddProduct';
+import DeleteProduct from '../Components/DeleteProduct';
+import UpdateProduct from '../Components/UpdateProduct';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
         {/* <Route path="/" element={<Navbar/>}></Route> add homepage here */}
         <Route path="/login" element={<Login/>}></Route>
         <Route path='/signup' element={ <SignUp/>}></Route>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/deleteproduct" element={<DeleteProduct />} />
+        <Route path="/updateproduct" element={<UpdateProduct />} />
+        
       </Routes>
     </BrowserRouter>
       
